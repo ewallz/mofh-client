b9cfb8e6822fa47dcbfb64dadd5aeb7ef3c7b2ec
Hostronavt Team <info@hostroteam.ru>
2019-07-23 15:17:24 +0300

    Update lib.php

--- lib/lib.php
+++ lib/lib.php
@@ -38,7 +38,9 @@ class Panel {
         $this->login = $login;
         $this->password = $password;
         $gettry = $this->handle('panel/index.php');
-	$this->page = $gettry;
+	$htmltry = new simple_html_dom();
+        $htmltry->load($gettry);
+        $this->page = $htmltry;
         if (strpos($gettry, 'Go To Login')) {
         $get = $this->handle('index.php', array('uname' => $login, 'passwd' => $password,'role' => 'administrator','submit' => 'Login'));
         preg_match_all("/\\nLocation:\\s*(.*?)\\n/i", $get, $matches);
